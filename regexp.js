50254491ac9ca2e850d2c033119fedc712b7bb02
lolly <gratapaula@gmail.com>
2021-09-25 10:54:46 -0500

    first commit

---
+++ regexp.js
@@ -0,0 +1,17 @@
+
+function searchtext() {
+
+    x = document.getElementById("xID").value;
+    y = document.getElementById("yID").value;
+
+    var str = x;
+    var patt = new RegExp(y);
+    var res = patt.test(str);
+    if (res===true){
+        res = "text contains search"
+    } else{
+        res = "text does not contain search"
+    }
+    document.getElementById("n").innerHTML = res;
+}
+
